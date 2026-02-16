0e392057f8ece388b83c29a3c938b617044e49b2
Aniket Raj <araj52444@gmail.com>
2026-02-16 13:45:25 +0530

    refactor: clean up Products and Search components for improved readability and consistency

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo } from "react";
+import { useMemo } from "react";
 import useDebounce from "../hooks/useDebounce";
 
 const products = [
@@ -22,33 +22,25 @@ const Products = ({ filter }) => {
   }, [delayedData]);
   return (
     <div className="w-full h-full flex flex-col items-center gap-y-10">
-      <h1 className="text-4xl font-bold tracking-tight">Products</h1>
-      <ul className="w-full max-w-xl">
-        {results.map((product, index) => (
-          <li
-            className="border rounded-sm px-4 py-2 mb-4 last:mb-0"
-            key={index}
-          >
-            {product}
-          </li>
-        ))}
-      </ul>
+      <h1 className="text-4xl font-bold tracking-tight">
+        {delayedData ? `Results for ${delayedData}` : "All Products"}
+      </h1>
+      {results.length === 0 ? (
+        <div>No result found for {delayedData}</div>
+      ) : (
+        <ul className="w-full max-w-xl">
+          {results.map((product, index) => (
+            <li
+              className="border rounded-sm px-4 py-2 mb-4 last:mb-0"
+              key={index}
+            >
+              {product}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
 
 export default Products;
-
-
-
-// export const Test = () => {
-
-//     useCallback(() => {
-//       //logic  
-//     },[])
-
-
-//   return (
-//     <div>Test</div>
-//   )
-// }
--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,23 +1,23 @@
 import { useState } from "react";
 import { Search as SearchIcon } from "lucide-react";
 
-const Search = ({setQuery}) => {
+const Search = ({ setQuery }) => {
   const [search, setSearch] = useState("");
 
   const handleSearch = (e) => {
     setSearch(e.target.value);
-    setQuery(e.target.value)
-  }
+    setQuery(e.target.value);
+  };
 
   return (
-    <div className="focus-within:border-2 focus-within:border-blue-400 flex shadow-lg shadow-gray-300 px-4 py-2 w-1/3 max-w-2xl rounded-2xl gap-2">
+    <div className="focus-within:border focus-within:ring-1 focus-within:ring-blue-400 focus-within:border-blue-400 flex shadow-lg focus-within:shadow-gray-300/80 px-4 py-2 w-1/3 max-w-2xl rounded-2xl gap-2">
       <input
-        type="text" 
-        className="w-full outline-0 placeholder:text-sm placeholder:text-gray-400" 
-        value={search} 
+        type="text"
+        className="w-full outline-0 placeholder:text-sm placeholder:text-gray-400"
+        value={search}
         onChange={handleSearch}
-        placeholder="Write your query..."
-    />
+        placeholder="Type to Search..."
+      />
       <SearchIcon />
     </div>
   );
